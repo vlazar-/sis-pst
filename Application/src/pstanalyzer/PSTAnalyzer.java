a2b8db97b1c0e834052d1771d8a38f5c7cd82ac6
Viktor Lazar <vlazar@outlook.com>
2014-12-07 18:10:54 +0100

    app

---
+++ Application/src/pstanalyzer/PSTAnalyzer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pstanalyzer;
+
+import com.pff.PSTFile;
+import gui.MainForm;
+
+/**
+ *
+ * @author Matej
+ */
+public class PSTAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainForm form = new MainForm();
+        form.setVisible(true);
+        
+        //PSTFile pstFile = new PSTFile("");
+    }
+    
+}
