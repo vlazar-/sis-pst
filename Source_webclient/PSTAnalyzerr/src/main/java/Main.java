4fa4475aba952d169bb20770d33e02195c6c3b5e
Viktor Lazar <vlazar@udruga-mis.hr>
2015-01-11 15:44:37 +0100

    File chooser on main Swing form

--- Source_webclient/PSTAnalyzerr/src/main/java/Main.java
+++ Source_webclient/PSTAnalyzerr/src/main/java/Main.java
@@ -4,9 +4,13 @@ import ElasticSearch.DbConnect;
 import PST.PSTFileEmail;
 import com.pff.PSTFile;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-
+import java.io.File;
 
 
 /**
@@ -14,57 +18,115 @@ import java.awt.event.KeyEvent;
  */
 public class Main extends JFrame {
 
-    protected JButton btnStart, btnStop;
-    protected JLabel labStart;
-    private static final int DEFAULT_SIZE = 3;
-    protected Dimension dimension = new Dimension(600, 400);
-
+    static String fileName;
+    JButton btnStart, btnSearch;
+    JLabel labFile, labLaunch, labStatus;
+    JTextField displayField;
 
     public Main() {
 
-        JPanel pane = (JPanel) getContentPane();
-        pane.setPreferredSize(dimension);
-        GroupLayout gl = new GroupLayout(pane);
-        pane.setLayout(gl);
+        JFrame frame = new JFrame("PSTAnalyzerr");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
 
-        labStart = new JLabel();
-        System.out.println(labStart.getText());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(4, 4, 4, 4);
+        gbc.gridx = 0;
+        gbc.weightx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = gbc.CENTER;
 
+        gbc.gridwidth = 3;
+        frame.add(createPane(frame.getBackground()), gbc);
+
+        gbc.gridy = 1;
+        gbc.gridx = 0;
+        gbc.gridwidth = 1;
+        labFile = new JLabel("1. Load .pst file: ");
+        frame.add(labFile, gbc);
+
+        gbc.gridx = 1;
+        displayField = new JTextField(30);
+        displayField.setEditable(false);
+        frame.add(displayField, gbc);
+
+        gbc.gridx = 2;
+        btnSearch = new JButton("...");
+        frame.add(btnSearch, gbc);
+
+        gbc.gridy = 2;
+        gbc.gridx = 0;
+        labLaunch = new JLabel("2. Launch client: ");
+        frame.add(labLaunch, gbc);
+
+        gbc.gridx = 2;
         btnStart = new JButton("Launch");
-        btnStart.setMnemonic(KeyEvent.VK_M);
-        btnStart.addActionListener(e -> {
-            System.out.println("klik");
-            initElasticSearch();
-            labStart.setText("  ElasticSearch is up and running  ");
-            Server s = new Server();
-            s.initServer();
-            labStart.setText(labStart.getText() + " -   Go to localhost:4567 ");
+        btnStart.setEnabled(false);
+        frame.add(btnStart, gbc);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        gbc.gridwidth = 3;
+        frame.add(createPane(frame.getBackground()), gbc);
+
+        gbc.gridy = 4;
+        gbc.gridx = 0;
+        gbc.gridwidth = 3;
+        labStatus = new JLabel("Status: Waiting for .pst file");
+        frame.add(labStatus, gbc);
+
+        /**
+         * Action for ... button
+         */
+        btnSearch.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                JFileChooser openFile = new JFileChooser();
+
+                //FileFilter type = new FileNameExtensionFilter(".pst", ".pst");
+                //openFile.setFileFilter(type);
+                openFile.showOpenDialog(null);
+
+                File selectedPst = openFile.getSelectedFile();
+                displayField.setText(selectedPst.getAbsolutePath());
+                fileName = selectedPst.getAbsolutePath();
+                btnStart.setEnabled(true);
+            }
         });
 
+        /**
+         * Action for launch button
+         */
+        btnStart.setMnemonic(KeyEvent.VK_M);
+        btnStart.addActionListener(e -> {
+                    System.out.println("Client launch started..");
+                    initElasticSearch();
+                    labStatus.setText("Status:  ElasticSearch is up and running  ");
+                    Server s = new Server();
+                    s.initServer();
+                    labStatus.setText(labStatus.getText() + " - Point your browser to localhost:4567");
+                });
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-        //gl.setAutoCreateContainerGaps(true);
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-                        .addComponent(btnStart)
-                        .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(labStart))
-        );
+    }
+    public JPanel createPane(Color color) {
+        JPanel pane = new JPanel(){
 
-        gl.setVerticalGroup(gl.createSequentialGroup()
-                        .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(btnStart)
-                                .addComponent(labStart))
-        );
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(20, 20);
+            }
 
-        pack();
-        //center of screen
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        };
+        pane.setBackground(color);
+        return pane;
     }
 
     public static void initElasticSearch() {
-        // TODO code application logic here
-
-        String fileName = "C:" + "\\" + "Users" + "\\" + "martina" + "\\" + "Documents" + "\\" + "GitRepos" + "\\" + "sis-pst" + "\\" +"PST_files" + "\\" + "goran_fazer@hotmail.com.pst";
         System.out.println(fileName);
         CreateNode createNode = CreateNode.getInstance();
         System.out.println("CLIENT DATA MOTHERFUCKER!!!!!: "+createNode.node.client().toString());
@@ -94,7 +156,6 @@ public class Main extends JFrame {
 
     private static void initGUI() {
         Main m = new Main();
-        m.setVisible(true);
     }
 
     public static void main(String[] args) {
