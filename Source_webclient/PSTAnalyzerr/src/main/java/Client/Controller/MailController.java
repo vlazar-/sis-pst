59d5759ae5cee893fddd4ccfa4e8b90e80dcdeaf
Viktor Lazar <vlazar@udruga-mis.hr>
2015-01-18 21:33:58 +0100

    Email preview, fixed links, new ui

--- Source_webclient/PSTAnalyzerr/src/main/java/Client/Controller/MailController.java
+++ Source_webclient/PSTAnalyzerr/src/main/java/Client/Controller/MailController.java
@@ -13,9 +13,29 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class MailController {
 
     public MailController(final MailService mailService) {
+        /**
+         * Get mail from .pst
+         */
         get("/mail", (request, response) -> new ModelAndView(mailService.index(), "pages/mail.mustache"), new MustacheTemplateEngine());
-
         get("/mail-raw", (request, response) -> mailService.index());
+        /**
+         * Get mail from INBOX folder
+         */
+        get("/mail-inbox", (request, response) -> new ModelAndView(mailService.getFolder("Inbox"), "pages/mail.mustache"), new MustacheTemplateEngine());
+        get("/mail-inbox-raw", (request, response) -> mailService.getFolder("Inbox"));
+        /**
+         * Get mail from SENT folder
+         */
+        get("/mail-sent", (request, response) -> new ModelAndView(mailService.getFolder("Sent"), "pages/mail.mustache"), new MustacheTemplateEngine());
+        get("/mail-sent-raw", (request, response) -> mailService.getFolder("Sent"));
+        /**
+         * Get mail from TRASH folder
+         */
+        get("/mail-trash", (request, response) -> new ModelAndView(mailService.getFolder("Trash"), "pages/mail.mustache"), new MustacheTemplateEngine());
+        get("/mail-trash-raw", (request, response) -> mailService.getFolder("Trash"));
+
+        //get("/mail-single/:id", (request, response) -> mailService.getSingle(request.params(":id")));
+        get("/mail-single/:id", (request, response) -> new ModelAndView(mailService.getSingle(request.params(":id")), "pages/mail-single.mustache"), new MustacheTemplateEngine());
 
         get("/API/v1/getEmail/:id", (request, response) -> new GetEmail().getEmail(request.params(":id")));
         get("/API/v1/getEmailFromKeyword/:keyword", (request, response) -> new GetEmail().getEmail(request.params(":id")));
--- Source_webclient/PSTAnalyzerr/src/main/java/Client/Service/MailService.java
+++ Source_webclient/PSTAnalyzerr/src/main/java/Client/Service/MailService.java
@@ -2,7 +2,6 @@ package Client.Service;
 
 
 import Client.JSONParser;
-
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
@@ -24,22 +23,22 @@ public class MailService {
                 .Builder("http://localhost:9200/")
                 .multiThreaded(true)
                 .build());
-        JestClient clienttt = factory.getObject();
-        String upit = "{\n" +
+        JestClient jestClient = factory.getObject();
+        String query = "{\n" +
                 " \"size\": 5000, " +
                 "   \"query\": {\n" +
                 "      \"match_all\": {}\n" +
                 "   }\n" +
                 "}";
 
-        Search search = new Search.Builder(upit).addIndex("pstindex").build();
+        Search search = new Search.Builder(query).addIndex("pstindex").build();
 
         try {
-            io.searchbox.core.SearchResult result = clienttt.execute(search);
-            System.out.println("MAIL SERVICE:" + result.getJsonString());
+            io.searchbox.core.SearchResult result = jestClient.execute(search);
+            System.out.println("MAIL SERVICE - index:" + result.getJsonString());
             // mail.put("result", parseJson(result.getJsonString()));
             // return mail;
-            return parseJson(result.getJsonString());
+            return parseResponse(result.getJsonString(), "mail");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,25 +46,90 @@ public class MailService {
         return null;
     }
 
-    public Map parseJson(String jsonString) {
-            //JsonArray result = new JsonArray();
-            JSONArray re = new JSONArray();
+    public Map getFolder(String folder){
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig
+                .Builder("http://localhost:9200/")
+                .multiThreaded(true)
+                .build());
+        JestClient jestClient = factory.getObject();
+        String query = "{\n" +
+                " \"size\": 5000, " +
+                "   \"query\": {\n" +
+                "       \"query_string\": {"+
+                "           \"query\": \"folderName:"+ folder +"\" \n" +
+                "        }\n"+
+                "   }\n" +
+                "}";
 
-            JSONObject a = new JSONObject(jsonString);
-            a = a.getJSONObject("hits");
-            JSONArray b = a.getJSONArray("hits");
+        Search search = new Search.Builder(query).addIndex("pstindex").build();
+
+        try {
+            io.searchbox.core.SearchResult result = jestClient.execute(search);
+            System.out.println("MAIL SERVICE -folder( " + folder + " ):" + result.getJsonString());
+            // mail.put("result", parseJson(result.getJsonString()));
+            // return mail;
+            return parseResponse(result.getJsonString(), "mail");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Map getSingle(String id){
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig
+                .Builder("http://localhost:9200/")
+                .multiThreaded(true)
+                .build());
+        JestClient jestClient = factory.getObject();
+        String query = "{ \n" +
+                "\"size\": 1, \n" +
+                "\"query\": { \n" +
+                "    \"query_string\": { \n" +
+                "       \"query\": \n" +
+                "           \"internetMessageId:" + id + "\" \n" +
+                "        }\n"+
+                "   }\n" +
+                "}";
+
+        Search search = new Search.Builder(query).addIndex("pstindex").build();
+
+        try {
+            io.searchbox.core.SearchResult result = jestClient.execute(search);
+            System.out.println("MAIL SERVICE - single( " + id + " ):" + result.getJsonString());
+            // mail.put("result", parseJson(result.getJsonString()));
+            // return mail;
+            return parseResponse(result.getJsonString(), "single");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public Map parseResponse(String jsonString, String responseName) {
+        //JsonArray result = new JsonArray();
+        JSONArray re = new JSONArray();
+
+        JSONObject a = new JSONObject(jsonString);
+        a = a.getJSONObject("hits");
+        JSONArray b = a.getJSONArray("hits");
 
 
-            for(int i=0; i<b.length(); i++){
-                JSONObject t = b.getJSONObject(i);
-                t = t.getJSONObject("_source");
-                re.put(t);
-            }
+        for(int i=0; i<b.length(); i++){
+            JSONObject t = b.getJSONObject(i);
+            t = t.getJSONObject("_source");
+            re.put(t);
+        }
 
-            JSONObject fr = new JSONObject();
-            fr.put("mail", re);
+        JSONObject fr = new JSONObject();
+        fr.put(responseName, re);
 
-            return JSONParser.jsonToMap(fr);
+        return JSONParser.jsonToMap(fr);
 
             /*JsonElement jsonElement = new JsonParser().parse(jsonString);
             JsonObject jsonObject = jsonElement.getAsJsonObject();
